6f81af136bffda411e686e6db896d9cd061a1e18
Clem <iphona.paiement@hotmail.com>
2020-12-17 17:47:44 -0500

    Created Project List component and added content layout

---
+++ src/components/Projects-List.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+
+const GridList = styled.div`
+  width: 80%;
+  height 30vh;
+  background-color: pink;
+`
+
+export default function ProjectsList() {
+  return (
+  	<GridList>This is a grid list</GridList>
+  )
+}
