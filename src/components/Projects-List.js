391ba878fda0ccb2cfb9bfb817cc652c8498002f
Clem <iphona.paiement@hotmail.com>
2020-12-20 21:46:48 -0500

    Added objects to projects array

--- src/components/Projects-List.js
+++ src/components/Projects-List.js
@@ -28,7 +28,7 @@ const ProjectsList = () => {
         completed: false,
         estimatedEffortLevel: 6,
         actualEffortLevel: 0,
-        description: 'This is a desciption of project 1'
+        description: 'Sit elit nisi dolor exercitation est esse tempor dolore in labore cillum ea sit. Non magna dolor fugiat cupidatat sint ullamco nisi ut qui ex pariatur qui esse incididunt excepteur labore.Culpa sint in pariatur pariatur nisi exercitation in ut elit sunt ut. Nulla excepteur et officia id id qui ut nisi non ex reprehenderit cupidatat laboris. Qui labore in ullamco qui aliquip duis anim adipisicing magna veniam aliqua qui consectetur mollit magna nisi elit. Ut esse aute esse pariatur voluptate commodo elit adipisicing eu sit. Ad dolor irure anim culpa anim velit dolore reprehenderit excepteur aliquip aliquip esse nisi. Voluptate proident non consequat fugiat est nisi proident dolor cupidatat irure occaecat eiusmod veniam proident exercitation. Eu culpa laborum quis excepteur non adipisicing do ut irure tempor qui consequat elit est in ea non. Est aliqua anim do quis esse in veniam ex consectetur ex sit et aute deserunt in esse.'
       },
       {
         id: 2,
@@ -38,7 +38,87 @@ const ProjectsList = () => {
         completed: true,
         estimatedEffortLevel: 8,
         actualEffortLevel: 10,
-        description: 'This is a description of project 2'
+        description: 'Sit elit nisi dolor exercitation est esse tempor dolore in labore cillum ea sit. Non magna dolor fugiat cupidatat sint ullamco nisi ut qui ex pariatur qui esse incididunt excepteur labore.Culpa sint in pariatur pariatur nisi exercitation in ut elit sunt ut. Nulla excepteur et officia id id qui ut nisi non ex reprehenderit cupidatat laboris. Qui labore in ullamco qui aliquip duis anim adipisicing magna veniam aliqua qui consectetur mollit magna nisi elit. Ut esse aute esse pariatur voluptate commodo elit adipisicing eu sit. Ad dolor irure anim culpa anim velit dolore reprehenderit excepteur aliquip aliquip esse nisi. Voluptate proident non consequat fugiat est nisi proident dolor cupidatat irure occaecat eiusmod veniam proident exercitation. Eu culpa laborum quis excepteur non adipisicing do ut irure tempor qui consequat elit est in ea non. Est aliqua anim do quis esse in veniam ex consectetur ex sit et aute deserunt in esse.'
+      },
+      {
+        id: 3,
+        projectTitle: 'Project Title',
+        clientName: 'Michel Jamati',
+        dateCreated: '12 days ago',
+        completed: true,
+        estimatedEffortLevel: 8,
+        actualEffortLevel: 10,
+        description: 'Sit elit nisi dolor exercitation est esse tempor dolore in labore cillum ea sit. Non magna dolor fugiat cupidatat sint ullamco nisi ut qui ex pariatur qui esse incididunt excepteur labore.Culpa sint in pariatur pariatur nisi exercitation in ut elit sunt ut. Nulla excepteur et officia id id qui ut nisi non ex reprehenderit cupidatat laboris. Qui labore in ullamco qui aliquip duis anim adipisicing magna veniam aliqua qui consectetur mollit magna nisi elit. Ut esse aute esse pariatur voluptate commodo elit adipisicing eu sit. Ad dolor irure anim culpa anim velit dolore reprehenderit excepteur aliquip aliquip esse nisi. Voluptate proident non consequat fugiat est nisi proident dolor cupidatat irure occaecat eiusmod veniam proident exercitation. Eu culpa laborum quis excepteur non adipisicing do ut irure tempor qui consequat elit est in ea non. Est aliqua anim do quis esse in veniam ex consectetur ex sit et aute deserunt in esse.'
+      },
+      {
+        id: 4,
+        projectTitle: 'Project Title',
+        clientName: 'Michel Jamati',
+        dateCreated: '12 days ago',
+        completed: true,
+        estimatedEffortLevel: 8,
+        actualEffortLevel: 10,
+        description: 'Sit elit nisi dolor exercitation est esse tempor dolore in labore cillum ea sit. Non magna dolor fugiat cupidatat sint ullamco nisi ut qui ex pariatur qui esse incididunt excepteur labore.Culpa sint in pariatur pariatur nisi exercitation in ut elit sunt ut. Nulla excepteur et officia id id qui ut nisi non ex reprehenderit cupidatat laboris. Qui labore in ullamco qui aliquip duis anim adipisicing magna veniam aliqua qui consectetur mollit magna nisi elit. Ut esse aute esse pariatur voluptate commodo elit adipisicing eu sit. Ad dolor irure anim culpa anim velit dolore reprehenderit excepteur aliquip aliquip esse nisi. Voluptate proident non consequat fugiat est nisi proident dolor cupidatat irure occaecat eiusmod veniam proident exercitation. Eu culpa laborum quis excepteur non adipisicing do ut irure tempor qui consequat elit est in ea non. Est aliqua anim do quis esse in veniam ex consectetur ex sit et aute deserunt in esse.'
+      },
+      {
+        id: 5,
+        projectTitle: 'Project Title',
+        clientName: 'Michel Jamati',
+        dateCreated: '12 days ago',
+        completed: true,
+        estimatedEffortLevel: 8,
+        actualEffortLevel: 10,
+        description: 'Sit elit nisi dolor exercitation est esse tempor dolore in labore cillum ea sit. Non magna dolor fugiat cupidatat sint ullamco nisi ut qui ex pariatur qui esse incididunt excepteur labore.Culpa sint in pariatur pariatur nisi exercitation in ut elit sunt ut. Nulla excepteur et officia id id qui ut nisi non ex reprehenderit cupidatat laboris. Qui labore in ullamco qui aliquip duis anim adipisicing magna veniam aliqua qui consectetur mollit magna nisi elit. Ut esse aute esse pariatur voluptate commodo elit adipisicing eu sit. Ad dolor irure anim culpa anim velit dolore reprehenderit excepteur aliquip aliquip esse nisi. Voluptate proident non consequat fugiat est nisi proident dolor cupidatat irure occaecat eiusmod veniam proident exercitation. Eu culpa laborum quis excepteur non adipisicing do ut irure tempor qui consequat elit est in ea non. Est aliqua anim do quis esse in veniam ex consectetur ex sit et aute deserunt in esse.'
+      },
+      {
+        id: 6,
+        projectTitle: 'Project Title',
+        clientName: 'Michel Jamati',
+        dateCreated: '12 days ago',
+        completed: false,
+        estimatedEffortLevel: 6,
+        actualEffortLevel: 0,
+        description: 'Sit elit nisi dolor exercitation est esse tempor dolore in labore cillum ea sit. Non magna dolor fugiat cupidatat sint ullamco nisi ut qui ex pariatur qui esse incididunt excepteur labore.Culpa sint in pariatur pariatur nisi exercitation in ut elit sunt ut. Nulla excepteur et officia id id qui ut nisi non ex reprehenderit cupidatat laboris. Qui labore in ullamco qui aliquip duis anim adipisicing magna veniam aliqua qui consectetur mollit magna nisi elit. Ut esse aute esse pariatur voluptate commodo elit adipisicing eu sit. Ad dolor irure anim culpa anim velit dolore reprehenderit excepteur aliquip aliquip esse nisi. Voluptate proident non consequat fugiat est nisi proident dolor cupidatat irure occaecat eiusmod veniam proident exercitation. Eu culpa laborum quis excepteur non adipisicing do ut irure tempor qui consequat elit est in ea non. Est aliqua anim do quis esse in veniam ex consectetur ex sit et aute deserunt in esse.'
+      },
+      {
+        id: 7,
+        projectTitle: 'Project Title',
+        clientName: 'Michel Jamati',
+        dateCreated: '12 days ago',
+        completed: false,
+        estimatedEffortLevel: 6,
+        actualEffortLevel: 0,
+        description: 'Sit elit nisi dolor exercitation est esse tempor dolore in labore cillum ea sit. Non magna dolor fugiat cupidatat sint ullamco nisi ut qui ex pariatur qui esse incididunt excepteur labore.Culpa sint in pariatur pariatur nisi exercitation in ut elit sunt ut. Nulla excepteur et officia id id qui ut nisi non ex reprehenderit cupidatat laboris. Qui labore in ullamco qui aliquip duis anim adipisicing magna veniam aliqua qui consectetur mollit magna nisi elit. Ut esse aute esse pariatur voluptate commodo elit adipisicing eu sit. Ad dolor irure anim culpa anim velit dolore reprehenderit excepteur aliquip aliquip esse nisi. Voluptate proident non consequat fugiat est nisi proident dolor cupidatat irure occaecat eiusmod veniam proident exercitation. Eu culpa laborum quis excepteur non adipisicing do ut irure tempor qui consequat elit est in ea non. Est aliqua anim do quis esse in veniam ex consectetur ex sit et aute deserunt in esse.'
+      },
+      {
+        id: 8,
+        projectTitle: 'Project Title',
+        clientName: 'Michel Jamati',
+        dateCreated: '12 days ago',
+        completed: false,
+        estimatedEffortLevel: 6,
+        actualEffortLevel: 0,
+        description: 'Sit elit nisi dolor exercitation est esse tempor dolore in labore cillum ea sit. Non magna dolor fugiat cupidatat sint ullamco nisi ut qui ex pariatur qui esse incididunt excepteur labore.Culpa sint in pariatur pariatur nisi exercitation in ut elit sunt ut. Nulla excepteur et officia id id qui ut nisi non ex reprehenderit cupidatat laboris. Qui labore in ullamco qui aliquip duis anim adipisicing magna veniam aliqua qui consectetur mollit magna nisi elit. Ut esse aute esse pariatur voluptate commodo elit adipisicing eu sit. Ad dolor irure anim culpa anim velit dolore reprehenderit excepteur aliquip aliquip esse nisi. Voluptate proident non consequat fugiat est nisi proident dolor cupidatat irure occaecat eiusmod veniam proident exercitation. Eu culpa laborum quis excepteur non adipisicing do ut irure tempor qui consequat elit est in ea non. Est aliqua anim do quis esse in veniam ex consectetur ex sit et aute deserunt in esse.'
+      },
+      {
+        id: 9,
+        projectTitle: 'Project Title',
+        clientName: 'Michel Jamati',
+        dateCreated: '12 days ago',
+        completed: false,
+        estimatedEffortLevel: 6,
+        actualEffortLevel: 0,
+        description: 'Sit elit nisi dolor exercitation est esse tempor dolore in labore cillum ea sit. Non magna dolor fugiat cupidatat sint ullamco nisi ut qui ex pariatur qui esse incididunt excepteur labore.Culpa sint in pariatur pariatur nisi exercitation in ut elit sunt ut. Nulla excepteur et officia id id qui ut nisi non ex reprehenderit cupidatat laboris. Qui labore in ullamco qui aliquip duis anim adipisicing magna veniam aliqua qui consectetur mollit magna nisi elit. Ut esse aute esse pariatur voluptate commodo elit adipisicing eu sit. Ad dolor irure anim culpa anim velit dolore reprehenderit excepteur aliquip aliquip esse nisi. Voluptate proident non consequat fugiat est nisi proident dolor cupidatat irure occaecat eiusmod veniam proident exercitation. Eu culpa laborum quis excepteur non adipisicing do ut irure tempor qui consequat elit est in ea non. Est aliqua anim do quis esse in veniam ex consectetur ex sit et aute deserunt in esse.'
+      },
+      {
+        id: 10,
+        projectTitle: 'Project Title',
+        clientName: 'Michel Jamati',
+        dateCreated: '12 days ago',
+        completed: false,
+        estimatedEffortLevel: 6,
+        actualEffortLevel: 0,
+        description: 'Sit elit nisi dolor exercitation est esse tempor dolore in labore cillum ea sit. Non magna dolor fugiat cupidatat sint ullamco nisi ut qui ex pariatur qui esse incididunt excepteur labore.Culpa sint in pariatur pariatur nisi exercitation in ut elit sunt ut. Nulla excepteur et officia id id qui ut nisi non ex reprehenderit cupidatat laboris. Qui labore in ullamco qui aliquip duis anim adipisicing magna veniam aliqua qui consectetur mollit magna nisi elit. Ut esse aute esse pariatur voluptate commodo elit adipisicing eu sit. Ad dolor irure anim culpa anim velit dolore reprehenderit excepteur aliquip aliquip esse nisi. Voluptate proident non consequat fugiat est nisi proident dolor cupidatat irure occaecat eiusmod veniam proident exercitation. Eu culpa laborum quis excepteur non adipisicing do ut irure tempor qui consequat elit est in ea non. Est aliqua anim do quis esse in veniam ex consectetur ex sit et aute deserunt in esse.'
       }
     ]
   )
